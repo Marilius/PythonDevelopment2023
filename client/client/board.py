0fd60a8116727c19a8390f5fde77b5aa68d739fa
Marilius <Demon3643@gmail.com>
2023-06-10 04:04:48 +0300

    pawn and knight moves

--- client/client/board.py
+++ client/client/board.py
@@ -1,4 +1,8 @@
-from pieces import BasePiece
+from pieces import BasePiece, NotPieceException
+
+
+NUMS = '12345678'
+LETTERS = 'abcdefgh'
 
 
 class Board:
@@ -42,10 +46,67 @@ class Board:
                 self.prev_board = self.curr_board.copy()
 
     def get_possible_moves(self, i: str, j: str) -> list[tuple[str, str]]:
-        # TODO(marilius): en passant, рокировка
-        ...
+        # TODO(marilius): en passant, рокировка, шах
+        if not isinstance(self.curr_board[(i, j)], BasePiece):
+            raise NotPieceException
 
-    def move(self, i0: str, j0: str, i1: str, j1: str) -> str:
+        piece = self.curr_board[(i, j)]
+        piece_name = piece.name
+        color = piece.color
+        possible_moves = []
+
+        match piece_name:
+            case 'p':  # pawn TODO(marilius): en passant
+                if color == 'w':
+                    d = 1
+                else:
+                    d = -1
+                if not piece.moved:
+                    possible_moves.append((i, NUMS[NUMS.find(j) + d * 2]))
+                possible_moves.append((i, NUMS[NUMS.find(j) + d * 1]))
+
+                pos_j0 = NUMS.find(j) + d * 1
+
+                pos_i0 = LETTERS.find(i) - 1
+                if 0 <= pos_i0 <= 7:
+                    if isinstance(self.curr_board[(LETTERS[pos_i0], NUMS[pos_j0])], BasePiece):
+                        if self.curr_board[(LETTERS[pos_i0], NUMS[pos_j0])].color == 'b':
+                            possible_moves.append((LETTERS[pos_i0], NUMS[pos_j0]))
+
+                pos_i0 = LETTERS.find(i) + 1
+                if 0 <= pos_i0 <= 7:
+                    if isinstance(self.curr_board[(LETTERS[pos_i0], NUMS[pos_j0])], BasePiece):
+                        if self.curr_board[(LETTERS[pos_i0], NUMS[pos_j0])].color == 'b':
+                            possible_moves.append((LETTERS[pos_i0], NUMS[pos_j0]))
+            case 'N':
+                pos_i, pos_j = LETTERS.find(i), NUMS.find(j)
+
+                possible_moves = []
+
+                for d0 in [-1, 1]:
+                    for d1 in [-1, 1]:
+                        if 0 <= pos_i + d0 * 1 <= 7 and 0 <= pos_j + d1 * 2 <= 7:
+                            possible_moves.append((LETTERS[pos_i + d0 * 1], NUMS[pos_j + d1 * 2]))
+                        if 0 <= pos_i + d0 * 2 <= 7 and 0 <= pos_j + d1 * 1 <= 7:
+                            possible_moves.append((LETTERS[pos_i + d0 * 2], NUMS[pos_j + d1 * 1]))
+
+        def f(x) -> bool:
+            i, j = x
+
+            # фигура того же цвета
+            if isinstance(self.curr_board[(i, j)], BasePiece):
+                if self.curr_board[(i, j)].color == color:
+                    return False
+
+            return True
+
+        possible_moves = list(filter(f, possible_moves))
+
+        ans = possible_moves
+        print(ans)
+        return ans
+
+    def move(self, i0: str, j0: str, i1: str, j1: str, promote: str = '') -> str:
         if (i1, j1) in self.get_possible_moves(i0, j0):
             name0 = self.curr_board[(i0, j0)].piece_name
 
@@ -56,12 +117,18 @@ class Board:
                 name1 = ''
                 sym = '-'
 
-            # TODO(marilius): check, promote
+            # TODO(marilius): check
             check = ''
-            promote = ''
 
             # TODO(marilius): Castling 0-0, 0-0-0
-            return f'{name0}{i0}{j1}{sym}{name1}{i1}{j1}{check}{promote}'
+
+            self.prev_board = self.curr_board.copy()
+            self.curr_board[(i1, j1)] = self.curr_board[(i0, j0)]
+            self.curr_board[(i0, j0)] = ' '
+
+            self.curr_board[(i1, j1)].move()
+
+            return f'{name0}{i0}{j0}{sym}{name1}{i1}{j1}{check}{promote}'
 
     def print(self) -> None:
         for j in reversed('12345678'):
@@ -71,4 +138,10 @@ class Board:
 
 
 test = Board()
+test.print()
+print(test.move('e', '2', 'e', '4'))
+test.print()
+print(test.move('e', '7', 'e', '5'))
+test.print()
+print(test.move('b', '1', 'c', '3'))
 test.print()
--- client/client/pieces.py
+++ client/client/pieces.py
@@ -2,10 +2,18 @@ class BasePiece:
     def __init__(self, name: str, color: str) -> None:
         self.name = name
         self.color = color
+        self.moved = False
 
     @property
     def piece_name(self) -> str:
         return self.name if self.name != 'p' else ''
 
+    def move(self) -> None:
+        self.moved = True
+
     def __str__(self) -> str:
         return self.name
+
+
+class NotPieceException(Exception):
+    ...
